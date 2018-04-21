3ed46487c0596c71771319c55af81d353666e8bc
Dan Weaver <danweaver06@gmail.com>
2018-04-21 09:16:08 -0500

    inital commit

---
+++ src/components/Box.ts
@@ -0,0 +1,9 @@
+import styled, { css } from "react-emotion";
+import { space, width, fontSize, color } from "styled-system";
+
+export const Box = styled("div")`
+    ${space}
+    ${width}
+    ${fontSize}
+    ${color}
+`;
